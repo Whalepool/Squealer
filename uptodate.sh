efb589eaa77ed88aad36774d9a2c762a2f5dc887
flibbr <flibbr@gmail.com>
2018-06-13 12:48:40 +0100

    Finalised uptodate.sh github checker

--- uptodate.sh
+++ uptodate.sh
@@ -1,6 +1,22 @@
 #!/bin/bash  
+msg_squealer() { 
+	SQUEALER_DIR=$(find /* -name squealer_pub.py -exec dirname {} \; 2>&1 | grep -v "Permission denied" )
+
+	if [[ -n $SQUEALER_DIR  ]]; then
+	    echo "Squealer is present on the server at $SQUEALER_DIR, msging $1"
+
+	    EXECUTE='python3.6 '"$SQUEALER_DIR"'/squealer_pub.py --msg="$1" --chat_id="61697695"'
+	    echo "executing: $EXECUTE"
+	    eval $EXECUTE
+
+	else
+		echo 'no'
+	fi
+}
+
 
 git remote -v update
+
 
 UPSTREAM=${1:-'@{u}'}
 LOCAL=$(git rev-parse @)
@@ -11,10 +27,16 @@ if [ $LOCAL = $REMOTE ]; then
     echo "Up-to-date"
 elif [ $LOCAL = $BASE ]; then
     echo "Need to pull"
+	msg_squealer "Pulling from github to update squealer"
     git pull 
-    
 elif [ $REMOTE = $BASE ]; then
+	msg_squealer "Squealer has changes that need to be pushed to github"
     echo "Need to push"
 else
+	msg_squealer "Squealer has divered from github main branch, merge then commit"
     echo "Diverged"
 fi
+
+
+
+
